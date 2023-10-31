f508470a03f853261ae96cf569fd22a959e8f804
Naveen Baskaran <naveen.baskaran@freshworks.com>
2023-10-31 17:54:51 +0530

    test init

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { screen, render, cleanup, fireEvent } from '@testing-library/react'
+import App from './App';
+
+
+describe('App component', () => {
+  beforeAll(() => {
+    render(<App />)
+  })
+
+  it('should have the right message in the dom', () => {
+    const message = 'Hello World';
+
+    expect(screen.getByText(message)).toBeInTheDocument()
+  })
+
+  afterAll(cleanup)
+})
